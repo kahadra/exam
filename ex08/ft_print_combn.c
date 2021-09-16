0adc900bab1d3bf71079117817064f573193b92e
Chunwoong Park <chpark@c1r1s2.42seoul.kr>
2021-09-16 15:06:06 +0900

    7

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -1,14 +1,14 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
 /*   ft_print_combn.c                                   :+:      :+:    :+:   */
-#                                                     +:+ +:+         +:+      #
-#    By: chpark <chpark@student.42seoul.kr>         +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/09/14 13:43:07 by chpark            #+#    #+#              #
-/*   Updated: 2021/09/16 14:38:48 by chpark           ###   ########.fr       */
-#                                                                              #
-# **************************************************************************** #
+/*                                                    +:+ +:+         +:+     */
+/*   By: chpark <chpark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 14:43:05 by chpark            #+#    #+#             */
+/*   Updated: 2021/09/16 14:59:25 by chpark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
@@ -17,46 +17,46 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_num(int *tab, int n);
+void	ft_print_num(int *tab, int n)
 {
 	int	i;
 
 	if (tab[n - 1] == 0 || n >= 10 || n <= 0)
-		break ;
+		return ;
 	i = 0;
-	while (i <= (n-1))
+	while (i <= (n - 1))
 	{
 		ft_putchar(tab[i] + 48);
 		i++;
 	}
-	if (i > n && tap[0] != (10 - n))
+	if (i > n && tab[0] != (10 - n))
 	{
-		ft_putchar(",");
-		ft_putchar(" ");
+		ft_putchar(',');
+		ft_putchar(' ');
 	}
 }
 
 void	ft_print_combn(int n)
 {
-	int i;
-	int tab[10];
+	int	i;
+	int	tab[10];
 
 	i = 0;
 	while (i < n)
-		tab[i] = i;	
+		tab[i] = i;
 	while (tab[0] <= (10 - n) && n >= 1)
 	{
 		ft_print_num(tab, n);
 		tab[n - 1]++;
-		if (tab[i] >= tab[i+1])
+		if (tab[i] >= tab[i + 1])
 			break ;
 		i = n;
-		while(i && n > 1)
+		while (i && n > 1)
 		{
 			i--;
 			if (tab[i] > 9)
 			{
-				tab[i-1]++;
+				tab[i - 1]++;
 				tab[i] = 0;
 			}
 		}
@@ -65,6 +65,6 @@ void	ft_print_combn(int n)
 
 int	main(void)
 {
-	ft_print_combn(3);
+	ft_print_combn(2);
 	return (0);
 }
